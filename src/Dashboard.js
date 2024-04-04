335b65421f7714c2d9dbad4af86f5136ce7e6c5c
Quinn Sieja <parkerlewellyn@aarons-air.lan>
2024-04-04 15:03:46 -0500

    Removed dashboard redirect

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,14 +1,7 @@
 import Userfront from "@userfront/toolkit/react";
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+
 
 function Dashboard() {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if (!Userfront.user.uuid) navigate("/");
-  }, [navigate]);
-
   return <h1> Welcome {Userfront.user.name} </h1>;
 }
 
