feae97fac11f00cb2bbade291e65f30cb6e83ec5
Quinn Sieja <parkerlewellyn@aarons-air.lan>
2024-04-05 15:39:17 -0500

    Second attempt at no user redirect

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-
-import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { Outlet, useNavigate, useLocation} from "react-router-dom";
+import Userfront from "@userfront/toolkit/react";
 import "./App.css";
 
 function App() {
+  const navigate = useNavigate();
+  const location = useLocation();
+  let loc = location.pathname.substring(1);
+
+  useEffect(() => {
+    if(!Userfront.user.userUuid && loc != "/") {
+      navigate("/")
+    }
+  }, [navigate])
 
   return (
     <div className="App">
