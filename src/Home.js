08b99d5b0e26d75e2bb168e200f0f58f234bbfcf
Quinn Sieja <parkerlewellyn@aarons-air.lan>
2024-04-08 09:20:31 -0500

    Removed sign in form

--- src/Home.js
+++ src/Home.js
@@ -1,54 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import Userfront, { SignupForm, LoginForm } from "@userfront/toolkit/react";
+import Userfront, { LoginForm } from "@userfront/toolkit/react";
 import logo from "./images/Full-Logo-White.png";
 Userfront.init("xbpwwqmn");
 
 function Home() {
   const navigate = useNavigate();
-  const [formState, setForm] = useState(1);
-  const forms = [
-    {
-      form: (
-        <SignupForm
-          theme={{
-            colors: {
-              light: "#ffffff",
-              dark: "#f7941d",
-              accent: "#f7a848",
-              lightBackground: "#fdfdfd",
-              darkBackground: "#2d2d2d",
-            },
-            colorScheme: "dark",
-            fontFamily: "Avenir, Helvetica, Arial, sans-serif",
-            size: "default",
-            extras: { hideSecuredMessage: true },
-          }}
-        />
-      ),
-      text: "Or, log in",
-    },
-    {
-      form: (
-        <LoginForm
-          theme={{
-            colors: {
-              light: "#ffffff",
-              dark: "#f7941d",
-              accent: "#f7a848",
-              lightBackground: "#fdfdfd",
-              darkBackground: "#2d2d2d",
-            },
-            colorScheme: "dark",
-            fontFamily: "Avenir, Helvetica, Arial, sans-serif",
-            size: "default",
-            extras: { hideSecuredMessage: true },
-          }}
-        />
-      ),
-      text: "Or, sign up",
-    },
-  ];
 
   useEffect(() => {
     if(Userfront.user.userUuid) {
@@ -59,14 +16,21 @@ function Home() {
   return (
     <div className="home">
       <img src={logo} className="logo" alt="Renovations Delivered" />
-      {forms[formState].form}
-      <button
-        className="btn btn-secondary"
-        onClick={() => setForm(formState === 0 ? 1 : 0)}
-      >
-        {" "}
-        {forms[formState].text}{" "}
-      </button>
+      <LoginForm
+          theme={{
+            colors: {
+              light: "#ffffff",
+              dark: "#f7941d",
+              accent: "#f7a848",
+              lightBackground: "#fdfdfd",
+              darkBackground: "#2d2d2d",
+            },
+            colorScheme: "dark",
+            fontFamily: "Avenir, Helvetica, Arial, sans-serif",
+            size: "default",
+            extras: { hideSecuredMessage: true },
+          }}
+        />
     </div>
   );
 }
