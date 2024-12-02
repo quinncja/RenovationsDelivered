6153ca7778ccfcfcf2a5e6318f7c05ae41337b8a
Quinn Sieja <quinnwsieja@gmail.com>
2024-12-02 13:51:01 -0600

    homestate to show projects

--- src/pages/dashboardHome/DashboardHome.js
+++ src/pages/dashboardHome/DashboardHome.js
@@ -3,18 +3,20 @@ import useWelcomeText from "utils/hooks/useWelcomeText";
 import TrackedJobs from "./trackedJobs/TrackedJobs";
 import AddJobs from "./trackedJobs/AddJobs";
 import HomeWidgets from "../../widgets/homeWidgets/HomeWidgets";
+import { useHome } from "context/HomeContext";
 
 function DashboardHome() {
   const welcomeText = useWelcomeText();
   const { trackedJobs } = useTrackedJobs();
-  console.log(trackedJobs)
+  const { homeState } = useHome();
+
   return (
     <div className="dashboard-welcome">
       <h1> {welcomeText} </h1>
 
       <HomeWidgets />
       {trackedJobs ? (
-        trackedJobs.length > 0 ? (
+        trackedJobs.length > 0 || homeState === 'year' ? (
           <TrackedJobs jobs={trackedJobs} />
         ) : (
           <AddJobs />
