975162567cc1e9877c27e0a9dbf445e642d2332a
Quinn Sieja <quinnwsieja@gmail.com>
2024-12-20 12:08:16 -0600

    resized jobcost view for wide displays

--- src/pages/jobcost/JobCostDashboard.js
+++ src/pages/jobcost/JobCostDashboard.js
@@ -1,13 +1,77 @@
 import Breakdown from "widgets/jobWidgets/Breakdown/Breakdown";
 import CostItem from "./items/CostItem";
 import useIsAdmin from "utils/hooks/useIsAdmin";
+import { useEffect, useState } from "react";
 
 const { useSingle } = require("utils/hooks/useSingle");
 
 function JobCostDashboard(){
     const single = useSingle();
     const isAdmin = useIsAdmin();
+    
+    const [isWide, setIsWide] = useState(window.innerWidth > 2320);
 
+    useEffect(() => {
+      const handleResize = () => {
+        setIsWide(window.innerWidth > 2320);
+      };
+  
+      window.addEventListener('resize', handleResize);
+  
+      handleResize();
+  
+      return () => window.removeEventListener('resize', handleResize);
+    }, []);
+    
+    if(isWide){
+        return(
+            <div id="dashboard" className={`dashboard-dropzone`}>
+            <CostItem
+                key={"Status"}
+                id={"Status"}
+                type={"Status"}
+                single={single}
+            />
+            {isAdmin && 
+            <CostItem
+                key={"Client Breakdown"}
+                id={"Client Breakdown"}
+                type={"Client Breakdown"}
+                single={single}
+            />
+            }
+            <CostItem
+                key={"Cost Analysis"}
+                id={"Cost Analysis"}
+                type={"Cost Analysis"}
+                single={single}
+            />
+            <CostItem
+                key={"Margin"}
+                id={"Margin"}
+                type={"Margin"}
+                single={single}
+            />
+            <CostItem
+                key={"Change Orders"}
+                id={"Change Orders"}
+                type={"Change Orders"}
+                single={single}
+            />
+            <CostItem
+                key={"Financial Overview"}
+                id={"Financial Overview"}
+                type={"Financial Overview"}
+                single={single}
+            />
+            <Breakdown type={"Material"} />
+            <Breakdown type={"Labor"} />
+            <Breakdown type={"Subcontractors"} />
+            <Breakdown type={"WTPM"} />
+            </div>
+        )
+    }
+    
     return(
         <div id="dashboard" className={`dashboard-dropzone`}>
             <CostItem
@@ -40,6 +104,12 @@ function JobCostDashboard(){
                 single={single}
             />
             }
+            <CostItem
+                key={"Cost Analysis"}
+                id={"Cost Analysis"}
+                type={"Cost Analysis"}
+                single={single}
+            />
             <CostItem
                 key={"Change Orders"}
                 id={"Change Orders"}
